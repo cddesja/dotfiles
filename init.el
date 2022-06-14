5e2e0ccbf4427031ea7e2dbc7084467ba6dfd7e7
Christopher David Desjardins <cddesjardins@gmail.com>
2022-06-14 07:39:23 -0400

    updated org file to auto tangle to init.el. switched to modus vivendi.

--- init.el
+++ init.el
@@ -1,59 +1,231 @@
-(require 'org)
-(org-babel-load-file
- (expand-file-name "settings.org"
-                   user-emacs-directory))
+(setq inhibit-startup-screen t)
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; 
-;; Org-Mode
+;; Font lock keywords for syntactic highlighting:
+(setq ess-R-font-lock-keywords
+      '((ess-R-fl-keyword:keywords . t)
+	(ess-R-fl-keyword:constants . t)
+	(ess-R-fl-keyword:modifiers . t)
+	(ess-R-fl-keyword:fun-defs . t)
+	(ess-R-fl-keyword:assign-ops . t)
+	(ess-R-fl-keyword:%op% . t)
+	(ess-fl-keyword:fun-calls . t)
+	(ess-fl-keyword:numbers . t)
+	(ess-fl-keyword:operators . t)
+	(ess-fl-keyword:delimiters . t)
+	(ess-fl-keyword:=)
+	(ess-R-fl-keyword:F&T . t)))
+
+(setq ring-bell-function 'ignore)
+
+(tool-bar-mode -1)
+
+(scroll-bar-mode -1)
+
+(global-hl-line-mode 0)
+
+(add-to-list 'default-frame-alist
+	     '(font . "JetBrains Mono-15"))
+
+(setq ns-use-proxy-icon nil)
+(setq frame-title-format nil)
+
+(show-paren-mode 1)
+
+(require 'smartparens-config)
+
+(add-hook 'ess-mode-hook #'smartparens-mode)
+
+;; Remove Flymake support:
+(setq ess-use-flymake nil)
+;; Replace it (globally) by Flycheck:
+(use-package flycheck
+  :ensure t
+  :init
+  (global-flycheck-mode t))
+
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
+
+;; This is only needed once, near the top of the file
+(eval-when-compile
+  (require 'use-package))
+(setq use-package-always-ensure t)
+
+;;; For packaged versions which must use `require':
+(use-package modus-themes
+  :ensure
+  :init
+  ;; Add all your customizations prior to loading the themes
+  (setq modus-themes-italic-constructs t
+	modus-themes-bold-constructs t 
+	modus-themes-region '(bg-only no-extend)
+	modus-themes-mode-line '(accented borderless 4 1)
+	modus-themes-syntax '(yellow-comments)
+	modus-themes-subtle-line-numbers t
+	modus-themes-markup '(intense background)
+	modus-themes-paren-match '(intense)
+	modus-themes-region '(accented)
+	modus-themes-hl-line '(intense)
+	modus-themes-org-blocks '(tinted-background)
+	modus-themes-links '(bold italic background))
+
+
+  ;; Load the theme files before enabling a theme
+  (modus-themes-load-themes)
+  :config
+  ;; Load the theme of your choice:
+  (modus-themes-load-vivendi) ;; OR (modus-themes-load-vivendi)
+  :bind ("<f5>" . modus-themes-toggle))
+;; (load-theme 'solarized-selenized-dark t)
+
+(use-package rainbow-delimiters
+  :hook ((prog-mode . rainbow-delimiters-mode)
+         (org-mode . rainbow-delimiters-mode)
+         (ess-mode . rainbow-delimiters-mode)))
+
+;;   (require 'color)
+;;  (set-face-attribute 'org-block nil :background
+;;		      (color-lighten-name
+;;		       (face-attribute 'default :background) 50))
+;;  (set-face-background 'org-block-begin-line (color-lighten-name
+;;						(face-attribute 'default :background) 10))
 ;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(setq org-log-done 'time) ; log time when a task is completed
+;;  (set-face-background 'org-block-end-line (color-lighten-name
+;;					      (face-attribute 'default :background) 10))
 
-;; make org-mode to export to odt to docx
+(require 'org-bullets)
+(add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
+
+(org-babel-do-load-languages
+'org-babel-load-languages
+'((R . t)))
+
+(setq org-confirm-babel-evaluate nil)
+
+(setq org-log-done 'time)
+
 (setq org-odt-preferred-output-format "docx")
 
-;; change scaling of headers
-;(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- ;; '(org-block ((t (:background "#434c5e"))))
- ;'(org-level-1 ((t (:inherit outline-1 :height 1.5))))
- ;'(org-level-2 ((t (:inherit outline-2 :height 1.4))))
- ;'(org-level-3 ((t (:inherit outline-3 :height 1.3))))
- ;'(org-level-4 ((t (:inherit outline-4 :height 1.2))))
- ;'(org-level-5 ((t (:inherit outline-5 :height 1.1)))))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
+(require 'poly-R)
 
 ;; For LaTeX, to remove the default width=.9\linewidth, set the org-latex-image-default-width to empty string. By this way, the image will have its natural size.
 ;; To do that on the fly use the set-variable emacs command. Or to set this variable permanently, add the following line in your init.el : (setq org-latex-image-default-width "")
 (setq org-latex-image-default-width "")
 
-;; enable aspell
 (setq ispell-program-name "/usr/local/Cellar/aspell/0.60.8/bin/aspell")
 
-;; company mode
-(require 'company)
-(setq company-selection-wrap-around t
-      company-tooltip-align-annotations t
-      company-idle-delay 0.36
-      company-minimum-prefix-length 2
-      company-tooltip-limit 10)
-(add-hook 'after-init-hook 'global-company-mode)
-(define-key company-active-map (kbd "M-h") 'company-show-doc-buffer)
+(with-eval-after-load 'ess
+(cl-pushnew "|>" ess-R-assign-ops :test 'string=))
 
+(exec-path-from-shell-initialize)
 
+;; (use-package lsp-mode			;
+;;   :init
+;;   ;; set prefix for lsp-command-keymap (few alternatives - "C-l", "C-c l")
+;;   (setq lsp-keymap-prefix "C-c l")
+;;   :hook (;; replace XXX-mode with concrete major-mode(e. g. python-mode)
+;; 		    (ess-r-mode . lsp)
+;; 		    ;; if you want which-key integration
+;; 		    (lsp-mode . lsp-enable-which-key-integration)
+;; 		    )
+;;   :commands lsp)
+
+;; ;; optionally
+;; (use-package lsp-ui :commands lsp-ui-mode)
+;; ;; if you are helm user
+;; ;;(use-package helm-lsp :commands helm-lsp-workspace-symbol)
+;; ;; if you are ivy user
+;; (use-package lsp-ivy :commands lsp-ivy-workspace-symbol)
+;; ;;(use-package lsp-treemacs :commands lsp-treemacs-errors-list)
+
+;; ;; optionally if you want to use debugger
+;; ;;(use-package dap-mode)
+;; ;; (use-package dap-LANGUAGE) to load the dap adapter for your language
+
+;; ;; optional if you want which-key integration
+;; ;;(use-package which-key
+;; ;;    :config
+;; ;;    (which-key-mode))
+;; (with-eval-after-load 'lsp-mode  (lsp-register-client (make-lsp-client 
+;; 						       :new-connection (lsp-stdio-connection '("R" "--slave" "-e" "languageserver::run()"))
+;; 						       :major-modes '(ess-r-mode inferior-ess-r-mode)                    :server-id 'lsp-R)))
+
+(use-package ivy
+  :init
+  ;;(ivy-mode)
+  (setq ivy-use-virtual-buffers t)
+  (setq enable-recursive-minibuffers t)
+  (setq ivy-re-builders-alist '((t . ivy--regex-fuzzy)))
+  ;; enable this if you want `swiper' to use it
+  ;; (setq search-default-mode #'char-fold-to-regexp)
+  :bind (("C-s" . swiper)
+  ("C-c C-r" . ivy-resume)
+  ("<f6>" . ivy-resume)
+  ("M-x" . counsel-M-x)
+  ("C-x C-f" . counsel-find-file)
+  ("<f1> f" . counsel-describe-function)
+  ("<f1> v" . counsel-describe-variable)
+  ("<f1> o" . counsel-describe-symbol)
+  ("<f1> l" . counsel-find-library)
+  ("<f2> i" . counsel-info-lookup-symbol)
+  ("<f2> u" . counsel-unicode-char)
+  ("C-c g" . counsel-git)
+  ("C-c j" . counsel-git-grep)
+  ("C-c k" . counsel-ag)
+  ("C-x l" . counsel-locate)
+  :map minibuffer-local-map
+  ("C-r" . counsel-minibuffer-history))
+  :config
+  (ivy-mode 1))
+
+(use-package ivy-rich
+  :init
+  (ivy-rich-mode 1))
+
+(use-package which-key
+  :init (which-key-mode)
+  :diminish which-key-mode
+  :config
+  (setq which-key-idle-delay 1))
+
+(use-package helpful
+  :custom
+  (counsel-describe-function-function #'helpful-callable)
+  (counsel-describe-variable-function #'helpful-variable)
+  :bind
+  ([remap describe-function] . counsel-describe-function)
+  ([remap describe-command] . helpful-command)
+  ([remap describe-variable] . counsel-describe-variable)
+  ([remap describe-key] . helpful-key))
+
+(use-package evil
+  :config
+  (evil-mode 1))
+
+(mac-auto-operator-composition-mode)
+
+(use-package company
+  :bind (:map company-active-map
+	 ("C-n" . company-select-next)
+	 ("C-p" . company-select-previous)
+	 ("M-h" . company-show-doc-buffer))
+  :config
+  (setq company-idle-delay 0.3)
+  (setq company-selection-wrap-around t)
+  (setq company-tooltip-limit 10)
+  :hook
+   (after-init . global-company-mode))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(flx counsel zenburn-theme vterm use-package tron-legacy-theme spacemacs-theme solarized-theme smartparens rainbow-delimiters poly-R poet-theme org-bullets org nordless-theme nord-theme monokai-theme monokai-pro-theme modus-themes magit lsp-ui lsp-julia leuven-theme kaolin-themes julia-repl htmlize horizon-theme helm gruvbox-theme gotham-theme focus flycheck exec-path-from-shell ess dracula-theme doom-themes doom-modeline cyberpunk-theme cyberpunk-2019-theme company-quickhelp color-theme-sanityinc-tomorrow clues-theme berrys-theme)))
+ '(safe-local-variable-values
+   '((eval add-hook 'after-save-hook
+	   (lambda nil
+	     (org-babel-tangle))
+	   nil t))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
